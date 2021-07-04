1e12c55498d2217120056d250b617c76d685a78a
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-03 20:40:10 -0400

    all

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function linethroughList (){
-   const linethrough = document.querySelector("#arguments");
-   linethrough.strike() = "This is an argument."
+function linethroughList (){//Write a function that puts a line through the text of the first <li> in the unordered "Arguments" list (<ul). Since this function always does the same thing, it requires no parameters!
+   const lineThrough = document.querySelector("li");
+   const str = "This is an argument.";
+      lineThrough.style();
    
 }
+
+linethroughList()//call function linethroughList
+
+
