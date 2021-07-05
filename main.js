0ac387065010f22147039bed83f9ee9ccd38cd33
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-05 15:17:30 -0400

    all

--- main.js
+++ main.js
@@ -7,4 +7,57 @@ function linethroughList (){//Write a function that puts a line through the text
 
 linethroughList()//call function linethroughList
 
+//Write a function that takes in a string representing an DOM element's id and a string representing a url. The function should set the image with that id to have that url as its image source.
+function injestStringIDandUrl(stringID, wwwUrl){
+    const picImg = document.querySelector("image-1")
 
+picImg.src = "bamboo.png";
+
+
+
+}
+injestStringIDandUrl("image-1","bamboo.png");
+injestStringIDandUrl();
+injestStringIDandUrl();
+
+//Now use that function to set the three images in the Image Area section to show three images of your choice. You'll need three separate calls to your function!
+
+
+    const listRemove = document.querySelector("arguments")
+
+
+listRemove.remove();
+
+//Write a function that takes in two strings, one representing a font size and one representing an 
+
+function stringFont(string, font){
+    const adjustmentSize = document.querySelector("thing-c")
+    adjustmentSize.style.fontsize
+}
+
+//Part Two - DOM Elements as Function Parameters
+//These functions will take in DOM elements as parameters. They also don't need to return anything.
+
+//Write a function that takes in a DOM element and appends it to the Arguments <ul>.
+function appendArgument(){
+    const appArgu = document.querySelector("ul");
+    appArgu
+}
+
+//Write a function that takes in an image element and modifies its height to be 30 pixels.
+function imUpdate(){
+
+const imgChange = document.querySelector("img");
+
+imgChange.src = ;
+
+imgChange.height = ;
+}
+//Write a function that takes in an element and gives it the class invisible.
+
+function inVis(){
+const noSee = document.querySelector();
+noSee.invsiible
+}
+
+//
